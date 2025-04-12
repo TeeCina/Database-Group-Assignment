2427eece7990d41131c6f00b2f82a96f1035d127
TeeCina <mathandicina@petalmail.com>
2025-04-12 11:14:44 +0200

    Populated the address table with new data

---
+++ address_status.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM bookstore_db.address_status
+LIMIT 0, 1000
+
+-- Date: 2025-04-12 11:13
+*/
+INSERT INTO `` (`address_status_id`,`address_status_name`) VALUES (1,'Old');
+INSERT INTO `` (`address_status_id`,`address_status_name`) VALUES (2,'New');
