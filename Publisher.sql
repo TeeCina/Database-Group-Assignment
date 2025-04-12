f8bd696ad67204c8b7410e950c533641a909935f
TeeCina <mathandicina@petalmail.com>
2025-04-12 10:43:02 +0200

    Fixed naming of sql files and entities

--- Publisher.sql
+++ Publisher.sql
@@ -1,27 +1,27 @@
 USE bookstore_db;
 
-INSERT INTO publisher(publisher_id, publisher_name, publisher_email, publisher_contact)
+INSERT INTO publisher(publisher_id, publisher_name, publisher_email, publisher_phone)
 VALUE(74, "Lionel Ricardo", "lionelRic@publisher.com", 0112356987);
 
-INSERT INTO publisher(publisher_id, publisher_name, publisher_email, publisher_contact)
+INSERT INTO publisher(publisher_id, publisher_name, publisher_email, publisher_phone)
 VALUE(75, "Ellen Mathews", "mellen123@publisher.com", 0123456987);
 
-INSERT INTO publisher(publisher_id, publisher_name, publisher_email, publisher_contact)
+INSERT INTO publisher(publisher_id, publisher_name, publisher_email, publisher_phone)
 VALUE(76, "Niaima Noah", "noahN@publisher.com",0134567890);
 
-INSERT INTO publisher(publisher_id, publisher_name, publisher_email, publisher_contact)
+INSERT INTO publisher(publisher_id, publisher_name, publisher_email, publisher_phone)
 VALUE(77, "Titus Nzomo", "titusnzomo@publisher.com",0145678912);
 
-INSERT INTO publisher(publisher_id, publisher_name, publisher_email, publisher_contact)
+INSERT INTO publisher(publisher_id, publisher_name, publisher_email, publisher_phone)
 VALUE(78, "Thandeka Cina", "tcina4028@publisher.com",0156789123);
 
-INSERT INTO publisher(publisher_id, publisher_name, publisher_email, publisher_contact)
+INSERT INTO publisher(publisher_id, publisher_name, publisher_email, publisher_phone)
 VALUE(79, "Fezeka Plaatjie", "plaatjiefezi@publisher.com",0165478912);
 
-INSERT INTO publisher(publisher_id, publisher_name, publisher_email, publisher_contact)
+INSERT INTO publisher(publisher_id, publisher_name, publisher_email, publisher_phone)
 VALUE(80, "Mirriam Makhubela", "mmakhubela@publisher.com",0178945612);
 
-INSERT INTO publisher(publisher_id, publisher_name, publisher_email, publisher_contact)
+INSERT INTO publisher(publisher_id, publisher_name, publisher_email, publisher_phone)
 VALUE(81, "Lilly Dube", "madubeL@publisher.com",0189123654publisher);
 
 SELECT * FROM bookstore_db.publisher;
